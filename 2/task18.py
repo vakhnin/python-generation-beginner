ebd2f9d3410e1eac4cac42f20a3a2c9947b4fffc
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2026-03-29 14:50:52 +0300

    Решения

---
+++ 2/task18.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+sum = a + b
+sub = a - b
+mult = a * b
+
+print(a, "+", b, "=", sum)
+print(a, "-", b, "=", sub)
+print(a, "*", b, "=", mult)
---
+++ 2/task19.py
@@ -0,0 +1,6 @@
+a1 = int(input())
+d = int(input())
+n = int(input())
+
+an = a1 + d * (n - 1)
+print(an)
